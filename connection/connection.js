c95a8197834ae70173cf7274254f6ad053ab0f67
DawrAli33285 <lemightyeagle@gmail.com>
2026-06-01 02:16:30 +0500

    new

--- connection/connection.js
+++ connection/connection.js
@@ -1,6 +1,8 @@
 const mongoose=require('mongoose')
 
 
-const connection=mongoose.connect(`mongodb://127.0.0.1/propertymanagement`)
+// const connection=mongoose.connect(`mongodb://127.0.0.1/propertymanagement`)
+const connection=mongoose.connect(`mongodb+srv://twdnightowl_db_user:m3GTcSuQCLhi7jDL@cluster0.jjj7d4g.mongodb.net`)
+
 
 module.exports=connection
